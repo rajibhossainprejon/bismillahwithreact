5e4e04d649166c130bf98c1aad9201e75cb0045b
rajibhossainprejon <rajibhossainpijeon@gmail.com>
2020-04-19 01:42:44 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+const Header = () => {
+    return (
+        <div>
+            <h1>{logo}</h1>
+        </div>
+    );
+};
+
+export default Header;
